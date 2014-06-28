5abf6a233991b5f6c934a32c051b3ca4436d74da
Yichao Yu <yyc1992@gmail.com>
2014-06-28 10:29:36 +0800

    [readline] re-organize

--- readline/__init__.py
+++ readline/__init__.py
@@ -1,38 +1,7 @@
 # Copyright (C) 2014~2014 by Yichao Yu
 # yyc1992@gmail.com
 
-from ._cffi import _ffi, _lib
-import os
-
-try:
-    # 2to3 friendly
-    _unicode = eval('unicode')
-    _ffi_pystr = _ffi.string
-except:
-    _unicode = str
-    _bytes = bytes
-    def _ffi_pystr(s):
-        return _ffi.string(s).decode()
-else:
-    try:
-        _bytes = bytes
-    except:
-        _bytes = str
-
-
-def _to_cstr(s):
-    if isinstance(s, _unicode):
-        return s.encode()
-    return s
-
-
-def _to_cstr_null(s):
-    return _ffi.NULL if s is None else _to_cstr(s)
-
-
-def _handle_ioerr(err):
-    if err != 0:
-        raise IOError(err, os.strerror(err))
+from ._cffi import _ffi, _lib, _to_cstr, _to_cstr_null, _handle_ioerr
 
 
 class _ReadlineState(object):
--- readline/_cffi.py
+++ readline/_cffi.py
@@ -2,15 +2,43 @@
 # yyc1992@gmail.com
 
 from cffi import FFI
+
+import os
 from os import path as _path
+
+try:
+    # 2to3 friendly
+    _unicode = eval('unicode')
+    _ffi_pystr = _ffi.string
+except:
+    _unicode = str
+    _bytes = bytes
+    def _ffi_pystr(s):
+        return _ffi.string(s).decode()
+else:
+    try:
+        _bytes = bytes
+    except:
+        _bytes = str
 
 _ffi = FFI()
 _basedir = _path.dirname(__file__)
 
-
-def _get_api_header():
-    with open(_path.join(_basedir, 'readline_api.h')) as f:
-        return f.read()
-
-_ffi.cdef(_get_api_header())
+with open(_path.join(_basedir, 'readline_api.h')) as f:
+    _ffi.cdef(f.read())
 _lib = _ffi.dlopen(_path.join(_basedir, '_pyreadline.so'))
+
+
+def _to_cstr(s):
+    if isinstance(s, _unicode):
+        return s.encode()
+    return s
+
+
+def _to_cstr_null(s):
+    return _ffi.NULL if s is None else _to_cstr(s)
+
+
+def _handle_ioerr(err):
+    if err != 0:
+        raise IOError(err, os.strerror(err))
