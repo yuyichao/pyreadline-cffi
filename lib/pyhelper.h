c0ecc2ae11119c36dbe243c210166658875d9d64
Yichao Yu <yyc1992@gmail.com>
2014-06-28 10:47:52 +0800

    [pyhelper] callback

---
+++ lib/pyhelper.h
@@ -0,0 +1,8 @@
+#ifndef __PYHELPER_H
+#define __PYHELPER_H
+
+namespace py {
+void call(void*);
+}
+
+#endif
--- lib/readline_api.h
+++ lib/readline_api.h
@@ -6,3 +6,4 @@ void parse_and_bind(const char *s);
 int read_init_file(const char *s);
 int read_history_file(const char *s);
 int write_history_file(const char *s);
+void set_py_funcs(void (*)(void*));
